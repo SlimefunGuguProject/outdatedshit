5d5c15eace57526ed2fd8b6d35d40113c5e4a266
haiman <83174104+haiman233@users.noreply.github.com>
2022-06-18 12:00:58 +0800

    HANH1

--- src/main/java/io/github/terslenk/azap/utils/AddonItems.java
+++ src/main/java/io/github/terslenk/azap/utils/AddonItems.java
@@ -9,16 +9,16 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class AddonItems {
-    public static final SlimefunItemStack MAGTALIUM = new SlimefunItemStack("AZAP_MAGTALIUM", Material.MAGMA_CREAM,"&cMagtalium");
-    public static final SlimefunItemStack FIREPROOF_WAX = new SlimefunItemStack("AZAP_FIREPROOF_WAX",Material.HONEYCOMB_BLOCK,"&6Fireproof Wax");
+    public static final SlimefunItemStack MAGTALIUM = new SlimefunItemStack("AZAP_MAGTALIUM", Material.MAGMA_CREAM,"&c下界镁合金");
+    public static final SlimefunItemStack FIREPROOF_WAX = new SlimefunItemStack("AZAP_FIREPROOF_WAX",Material.HONEYCOMB_BLOCK,"&6防火蜡");
 
     public static void setup(Azap azap) {
         new FireproofWax(Category.AZAP_MAIN, FIREPROOF_WAX, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[] {
-                null,null,null,
+                null,new ItemStack(Material.HONEYCOMB),null,
                 null,new ItemStack(Material.MAGMA_CREAM),null,
-                null,null,null
+                null,new ItemStack(Material.HONEYCOMB),null
         }).register(azap);
-        new SlimefunItem(Category.AZAP_MAIN, MAGTALIUM, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[] {
+        new SlimefunItem(Category.AZAP_MAIN, MAGTALIUM, RecipeType.NULL, new ItemStack[] {
                 null,null,null,
                 null,null,null,
                 null,null,null
